21d86ced654b3c5ac40f484298c90652c4fb1a55
tom <tom@monksleigh.com>
2020-11-18 05:46:43 -0500

    updated

--- stars.py
+++ stars.py
@@ -1,22 +1,30 @@
-#from utils import *
+from utils import *
 import numpy as np
-from sklearn.metrics.pairwise import euclidean_distances
+import scipy.spatial.distance as sc
 
-r = 2
+# nums = np.random.randint(x0, x1, size = (n,3))
+# # for nearest neighbours and min/max dist apart maybe BallTree or KDTree? or some Nearest NEighbour calculation.
+# print(nums)
+# dis = sc.cdist(nums,nums)
+# print(dist)
+def rand_sep(n, x0, x1, y0, y1, z0, z1, d, seed = 256, test = 1000):
+  np.random.seed(seed)
+  for i in range(1,test):
+    x = np.random.randint(x0, x1, n)
+    y = np.random.randint(y0, y1, n)
+    z = np.random.randint(z0, z1, n)
+    points = np.column_stack((x,y,z))
+    #if (min(dist(nums)) >= d || max(dist(nums)) <= 6) {
+    if (min(sc.pdist(points)) < d):
+      return(points)
+  return("FAIL") #failed
 
-# Generate 100 points (3-tuples) between 0 and 10
-points = np.random.randint(0,100,[1000,3])
-
-# Pairwise distances between points
-distances = euclidean_distances(points)
-
-# "Remove" distance to itself by setting to a distance of r+1 (to discard it later)
-distances += np.identity(len(distances)) * (r+1)
-
-# Retrieve the distance to the closest point
-min_dist = np.min(distances,axis=1)
-
-# Filter your set of points
-filtered_points = points[min_dist>r]
-
-print(filtered_points)
+test = rand_sep(5000, 0, 100, 0, 100, 0, 100, 1)
+dist = sc.pdist(test)
+dmatrx =  sc.squareform(dist)
+np.savetxt("test.csv", test, delimiter=",")
+np.savetxt("dist.csv", dist, delimiter=",")
+np.savetxt("dmatrx.csv", dmatrx, delimiter=",")
+#print(test)
+#print(dist)
+#print(min(dist), max(dist))
