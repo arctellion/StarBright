641070ecbe21fe6a36e89a4ca89fb47f437283ba
tom <tom@monksleigh.com>
2020-11-24 12:51:56 -0500

    ammendments and first attempt at using functions

--- stars.py
+++ stars.py
@@ -1,16 +1,24 @@
 import numpy as np
 import pandas as pd
-import travtools as tt
+import travtools.stars as st
+import travtools.system as sy
 import plotly.express as px
 
-p  = tt.stars.Points(n=5000,r=25, center=(0,0,0), mindist=1)
+p  = st.Points(n=5000,r=25, center=(0,0,0), mindist=1)
 #print(p.points)
 #print(p)
 
-df = pd.DataFrame(p.points, columns=["x","y","z"])
-print(df.head())
+omega = pd.DataFrame(p.points, columns=["x","y","z"])
+omega = omega.rename_axis('id').reset_index()
+print(omega.head())
 
-fig = px.scatter_3d(df, x='x', y='y', z='z')
-fig.update_traces(marker=dict(size=2),
-                  selector=dict(mode='markers'))
-fig.show()
+
+omega['uwp'] = omega['id'].apply(st.fun_uwp)
+omega['pbg'] = omega['uwp'].apply(st.fun_pbg)
+
+print(omega.head())
+
+#fig = px.scatter_3d(df, x='x', y='y', z='z')
+#fig.update_traces(marker=dict(size=2),
+#                  selector=dict(mode='markers'))
+#fig.show()
