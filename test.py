e7c7d77bdf5c000d9cb127de725375b3ffd654c2
tom <tom@monksleigh.com>
2020-11-28 05:21:43 -0500

    testing networkx for drawing topographical star maps

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 #import plotly.express as px
 import pandas as pd
 import networkx as nx
+import scipy.spatial.distance as dis
 
 class Points():
     def __init__(self,n=10, r=1, center=(0,0,0), mindist=0.2, maxtrials=10000 ) :
@@ -45,55 +46,33 @@ class Points():
     def __repr__(self):
         return self.tx
 
-
 #center =(0,0,0)
 #radius = 25
 #mindist = 1
 
-p  = Points(n=50,r=10, center=(0,0,0), mindist=1)
-print(p.points)
+p  = Points(n=10,r=5, center=(0,0,0), mindist=5)
 #print(p)
 df = pd.DataFrame(p.points, columns=["x","y","z"])
-print(df.head())
+#print(df.head())
+a = dis.cdist(p.points,p.points)
 # translate df into dict for network
-pos = df.dict
+#pos = df.to_dict('index')
+#print(pos)
 
-G = nx.random_geometric_graph(50,4,pos=pos)
+print(a)
 
-pos = nx.get_node_attributes(G, 'pos')
-n = G.number_of_nodes()
-edge_max = max([G.degree(i) for i in range(n)])
-colors = [plt.cm.plasma(G.degree(i)/edge_max) for i in range(n)] 
-# 3D network plot
-with plt.style.context(('ggplot')):
+G = nx.from_numpy_matrix(a)
+plt.subplot(121)
+nx.draw(G)
+plt.savefig("path.png")
+#G = nx.drawing.nx_agraph.to_agraph(G)
 
-    fig = plt.figure(figsize=(10,7))
-    ax = Axes3D(fig)
+#G.node_attr.update(color="red", style="filled")
+#G.edge_attr.update(color="blue", width="2.0")
+#G.draw('out.png', format='png', prog='neato')
 
-    # Loop on the pos dictionary to extract the x,y,z coordinates of each node
-    for key, value in pos.items():
-        xi = value[0]
-        yi = value[1]
-        zi = value[2]
+#network_plot_3D(G,0)
 
-        # Scatter plot
-        ax.scatter(xi, yi, zi, c=colors[key], s=20+20*G.degree(key), edgecolors='k', alpha=0.7)
-
-    # Loop on the list of edges to get the x,y,z, coordinates of the connected nodes
-    # Those two points are the extrema of the line to be plotted
-    for i,j in enumerate(G.edges()):
-        x = np.array((pos[j[0]][0], pos[j[1]][0]))
-        y = np.array((pos[j[0]][1], pos[j[1]][1]))
-        z = np.array((pos[j[0]][2], pos[j[1]][2]))
-
-    # Plot the connecting lines
-        ax.plot(x, y, z, c='black', alpha=0.5)
-
-# Set the initial view
-ax.view_init(30, angle)
-# Hide the axes
-ax.set_axis_off()
-plt.show()
 #fig = px.scatter_3d(df, x='x', y='y', z='z')
 #fig.update_traces(marker=dict(size=2),
 #                  selector=dict(mode='markers'))
